35cc6bbd1cd06134617556d3783f8bdde4feb457
Thomas21119 <thomas.wittwer2018@gmail.com>
2021-09-06 12:48:46 +1000

    struggling with charAt

--- Develop/test.js
+++ Develop/test.js
@@ -10,11 +10,11 @@ slider.oninput = function () {
 
 var form = document.getElementById('form')
 //password Criteria
-var password = ['testing']
+var finalPassword = []
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 var numbers = ['1','2','3','4','5','6','7','8','9','0']
-var special = [' ','!','"', '#', '$', '%', '&', 'u0027','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','u005c',']','^','_','`','{','|','}','~']
+var special = [' ','!','"', '#', '$', '%', '&', '\u0027','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\u005c',']','^','_','`','{','|','}','~']
 
 
 var includeLowerCaseElement = document.getElementById('lowerCase')
@@ -25,31 +25,44 @@ var includeSpecialElement = document.getElementById('special')
 form.addEventListener('generate', e => {
   e.preventDefault()
   var sliderAmount = slider.value
-  var includeLowerCase =  includeLowerCaseElement.checked
-  var includeUpperCase = includeUpperCaseElement.checked
-  var includeNumbers = includeNumbersElement.checked
-  var includeSpecial = includeSpecialElement.checked
   var password = generatePassword(sliderAmount, includeLowerCase, includeUpperCase, includeNumbers, includeSymbols)
 })
 var writePasswordEL = document.getElementById('generate')
 var generatePasswordEL = document.getElementById('generate')
 
 generatePasswordEL.addEventListener('click', passwordContents)
-function passwordContents(sliderAmount, includeLowerCase, includeUpperCase, includeNumbers, includeSpecial) {
+function passwordContents(includeUpperCase, includeLowerCase, includeNumbers, includeSpecial) {
+  var includeLowerCase =  includeLowerCaseElement.checked
+  var includeUpperCase = includeUpperCaseElement.checked
+  var includeNumbers = includeNumbersElement.checked
+  var includeSpecial = includeSpecialElement.checked
+
+
+  let finalPassword = []
   if (includeLowerCase) {
-  password = password.concat(lowerCase)
-  console.log(password)
+  finalPassword = finalPassword.concat(lowerCase)
 }
   if (includeUpperCase) {
-  password = password.concat(upperCase)
+  finalPassword = finalPassword.concat(upperCase)
 }
   if (includeNumbers) {
-  password = password.concat(numbers)
+  finalPassword = finalPassword.concat(numbers)
 }  
   if (includeSpecial) {
   password = password.concat(special)
 }
-  console.log(password)
-}
+  function password(length) {
+    var result = "";
+    var characters = finalPassword
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+  console.log(password(10))
+  }
+
+ 
 
 
